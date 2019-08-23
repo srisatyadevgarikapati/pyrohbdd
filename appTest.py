b4f4f796a7d1208b59428b6da40cbb222ea570cf
srisatyadevgarikapati <39601383+srisatyadevgarikapati@users.noreply.github.com>
2019-08-23 00:29:23 -0500

    Update appTest.py

--- appTest.py
+++ appTest.py
@@ -3,5 +3,4 @@ import unittest
 def test_func_fast():
     pass
 if __name__ == '__main__':
-    import xmlrunner
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    unittest.main()
