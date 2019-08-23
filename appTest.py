253fdedfe867d1792bc220581560cf4516baf12c
srisatyadevgarikapati <39601383+srisatyadevgarikapati@users.noreply.github.com>
2019-08-23 00:49:27 -0500

    Update appTest.py

--- appTest.py
+++ appTest.py
@@ -3,5 +3,6 @@ import unittest
 class TestHello(unittest.TestCase):
         def setUp(self):
                 self.assertEqual("h","hasas")
+                with self.assertRaises(TypeError):
 if __name__ == '__main__':
     unittest.main()
